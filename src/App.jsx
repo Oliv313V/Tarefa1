c6cba75fa8882423a0c6b188205522331f6b8cfb
Oliv313V <146442775+Oliv313V@users.noreply.github.com>
2024-12-10 14:57:03 -0300

    React Projects - tarefa 1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className = "card"> 
+          <h1>Hello Wonderful World!</h1>
+          <p>
+            Espero que este dia te encontre com um sorriso no rosto e o coração leve.
+            O mundo está repleto de pequenas maravilhas que, muitas vezes, passam
+            despercebidas em nossa rotina, mas que têm o poder de trazer alegria e serenidade.
+            Que você consiga apreciar cada momento, seja nas pequenas coisas ou nos grandes desafios,
+            e que a gratidão sempre guie seus passos. Lembre-se de que, a cada amanhecer, temos
+            uma nova oportunidade de fazer a diferença e criar memórias que nos aquecerão por toda a vida.
+            <br />
+            A vida é uma jornada incrível, cheia de cores, sons e sentimentos que nos conectam uns aos outros.
+            Que, ao longo do dia, você se lembre de que é uma parte essencial deste belo e vasto universo.
+            Nunca subestime o poder de um gesto simples, de uma palavra de apoio ou de um olhar amigo. Vamos seguir
+            juntos, celebrando a beleza do nosso mundo e espalhando bondade por onde formos!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
